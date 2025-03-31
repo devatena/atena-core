5b14b86382add552919adae19968c44fe69db066
devatena <98615075+devatena@users.noreply.github.com>
2025-03-31 15:17:10 -0300

    remove imports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,2 @@
-import "../playground/src/themes/globalTheme"
 export * from "./components";
 import '../src/styles/global.css'
-export { default as ThemePlugin } from "./plugin"
-export { default as defaultTheme } from "./config/DefautTheme"
